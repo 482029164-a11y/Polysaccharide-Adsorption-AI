603da194290234281d15e7c7e3f9e53b98de4fa5
RIKA <482029164@qq.com>
2026-04-15 22:56:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ def load_dual_expert_system():
         # 常规流形载入 v6.2
         pack_normal = joblib.load('model_artifacts_v6_2.pkl')
         # 极限纠偏流形载入 v12
-        pack_penalty = joblib.load('model_artifacts_v12.pkl')
+        pack_penalty = joblib.load('model_artifacts_v16.pkl')
         
         tabm_normal = pack_normal['models']['True TabM']
         tabm_penalty = pack_penalty['models']['True TabM']
